8d355ba015f9d19ef319a2ca9370534918768829
sameerkaleem <160517211+sameerkaleem@users.noreply.github.com>
2025-01-10 16:37:39 +0500

    code updated

--- next-sanity-blog/sanity/lib/demo.ts
+++ next-sanity-blog/sanity/lib/demo.ts
@@ -1,5 +1,5 @@
 /**
- * Demo data used as placeholders and initial values for the blog
+ *  initial values for the blog
  */
 
 export const title = "Kitten-Blogs";
@@ -15,43 +15,10 @@ export const description = [
         marks: [],
         text: "Welocome to cute-kittens-blog ",
       },
-      // {
-      //   _key: "4a58edd077881",
-      //   _type: "span",
-      //   marks: ["ec5b66c9b1e0"],
-      //   text: "Next.js",
-      // },
-      // {
-      //   _key: "4a58edd077882",
-      //   _type: "span",
-      //   marks: [],
-      //   text: " and ",
-      // },
-      // {
-      //   _key: "4a58edd077883",
-      //   _type: "span",
-      //   marks: ["1f8991913ea8"],
-      //   text: "Sanity",
-      // },
-      // {
-      //   _key: "4a58edd077884",
-      //   _type: "span",
-      //   marks: [],
-      //   text: ".",
-      // },
+     
+  
     ],
-    // markDefs: [
-    //   {
-    //     _key: "ec5b66c9b1e0",
-    //     _type: "link",
-    //     href: "https://nextjs.org/",
-    //   },
-    //   {
-    //     _key: "1f8991913ea8",
-    //     _type: "link",
-    //     href: "https://sanity.io/",
-    //   },
-    // ],
+   
     style: "normal",
   },
 ];
